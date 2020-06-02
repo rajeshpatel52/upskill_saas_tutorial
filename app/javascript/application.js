5d5cbb8126348a2b038e190635a3e56dc1b8f8c6
rajesh patel <52rajeshpatel@gmail.com>
2020-06-02 14:50:21 +1200

    ammended application .js file in javascript

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -10,6 +10,8 @@ require("channels")
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap
+//= require turbolinks
+//= require_tree .
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
