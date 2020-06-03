66e98a6098a8ca65d4d72d50906cb48f3d80df5c
rajesh patel <52rajeshpatel@gmail.com>
2020-06-03 16:35:01 +1200

    added herouku and ammended contact_mailer.rb

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,5 +1,5 @@
 class ContactMailer < ActionMailer::Base
-  default to: 'rajeshpatel@linuxmail.org'
+  default to: '52rajeshpatel@gmail.com'
   def contact_email(name, email, body)
     @name = name
     @email = email
